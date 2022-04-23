87f6d8435c33098ebf1561a082b9ee5bed5b7bb4
rootCurse <mix.kras09@ya.ru>
2022-04-23 15:33:14 +0300

    lab-3 start of commit

--- kotiki-java/dataAccessObject/src/main/java/com/itmo/kotiki/entity/CatsEntity.java
+++ kotiki-java/dataAccessObject/src/main/java/com/itmo/kotiki/entity/CatsEntity.java
@@ -1,6 +1,7 @@
 package com.itmo.kotiki.entity;
 
-import javax.persistence.*;
+import jakarta.persistence.*;
+
 import java.sql.Date;
 
 @Entity
---
+++ kotiki-java/service/src/test/java/com/itmo/kotiki/service/HumanServiceTest.java
@@ -0,0 +1,39 @@
+package com.itmo.kotiki.service;
+
+import com.itmo.kotiki.entity.HumansEntity;
+import com.itmo.kotiki.repo.CatsRepository;
+import com.itmo.kotiki.repo.HumanRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.sql.Date;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+class HumanServiceTest {
+
+    @Test
+    void getName() {
+        var human = new HumansEntity("Ivan", new Date(1241225245));
+        var idOfHuman = human.getHumanId();
+        HumanRepository humanRepository = mock(HumanRepository.class);
+        when(humanRepository.getReferenceById(idOfHuman)).thenReturn(human);
+        var humanService = new HumanService(humanRepository);
+        String nameForAssert = humanService.getName(idOfHuman);
+        Assertions.assertEquals("Ivan", nameForAssert);
+    }
+
+    @Test
+    void getBirthday() {
+        Date date = new Date(1241225245);
+        var human = new HumansEntity("Ivan", date);
+        var idOfHuman = human.getHumanId();
+        HumanRepository humanRepository = mock(HumanRepository.class);
+        when(humanRepository.getReferenceById(idOfHuman)).thenReturn(human);
+        var humanService = new HumanService(humanRepository);
+        var dateForAssert = humanService.getBirthday(idOfHuman);
+        Assertions.assertEquals(date, dateForAssert);
+    }
+}
